0f474bf437eb602f8cbd4ff0692f9559efad2365
HanseeVee <166904840+HanseeVee@users.noreply.github.com>
2026-04-29 08:33:28 +0200

    Update agent.py

--- environment/src/CPRL/agent.py
+++ environment/src/CPRL/agent.py
@@ -1,50 +1,39 @@
 from typing import Type
 from stable_baselines3 import A2C, HER, PPO, DQN, DDPG, SAC, TD3  # Import the algorithm you trained on
 from sb3_contrib import ARS, MaskablePPO, QRDQN, RecurrentPPO, TQC, TRPO, CrossQ
+import gymnasium as gym
 from gymnasium import Env
+from mo_gymnasium.wrappers import LinearReward
 from pathlib import Path
 from stable_baselines3.common.base_class import BaseAlgorithm
 
-def initialize_agent_policy(agent_path: Path, env: Env, algo_str: str) -> BaseAlgorithm:
+def initialize_agent_policy(agent_path: Path, env: Env) -> BaseAlgorithm:
     """
         This function initializes the object that serves a policy. We use a policy from stablebaselines3.
         Feel free to replace the policy and tweak hyperparameters if needed.
     """
-    sb3_algorithms = {
-        "A2C": A2C,
-        "DDPG": DDPG,
-        "DQN": DQN,
-        "HER": HER,
-        "PPO": PPO,
-        "SAC": SAC,
-        "TD3": TD3,
-        "ARS": ARS,
-        "MaskablePPO": MaskablePPO,
-        "QRDQN": QRDQN,
-        "QR-DQN": QRDQN,
-        "QR_DQN": QRDQN,
-        "TRPO": TRPO,
-        "TQC": TQC,
-        "RPPO": RecurrentPPO,
-        "RECURRENTPPO": RecurrentPPO,
-        "CROSSQ": CrossQ
-    }
 
     agent_path = check_model_path(agent_path)
-    algo_obj = sb3_algorithms[algo_str.upper()]
-    model = algo_obj.load(agent_path, env=env)
+    model = DQN.load(agent_path, env=env)
     return model
 
-def check_model_path(agent_path: Path) -> Path:
+def initialize_agent_environment(env_name: str) -> Env:
     """
-    This function searches for the agent path including the upper directories.
+    This function initializes the gymnasium environment for your trained policy.
+    Feel free to apply different wrappers to your environment
     """
-    cur_dir = Path(__file__).resolve().parent
-    for _ in range(3):
-        full_path = cur_dir.joinpath(agent_path)
-        if full_path.exists():
-            return full_path
-        else:
-            cur_dir = cur_dir.parent
+    env = gym.make(env_name, render_mode=None)
+    if env_name.startswith("mo-"):
+        env = LinearReward(env)
+    return env
 
-    raise FileNotFoundError(f"{agent_path} not found.")
+
+def check_model_path(agent_str: str) -> Path:
+    """
+    This function searches for the agent path. It checks for this concrete path up to two directory levels above.
+    """
+    agent_path = Path(agent_str)
+    if agent_path.exists():
+        return agent_path
+    else:
+        raise FileNotFoundError(f"{agent_path} not found. Do you follow a path relative to the main directory?")
