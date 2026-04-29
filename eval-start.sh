a8c96a77d26ed4128ac05da9daa7f760da7df33d
Tobias Rempe <tobias.rempe@zyonic.de>
2026-04-29 16:01:52 +0200

    comments etc

--- eval-start.sh
+++ eval-start.sh
@@ -1,6 +1,15 @@
 # /bin/bash
-echo "Nothing is happening (yet)!"
+echo "Hi! Switching Directory!"
 
 cd $(cat /workdir)
+pwd
+
+echo "Checking current Leaderboard Order!"
 curl 10.150.4.148:5050/getorder
+
+
+echo "Running Eval-Script!"
 uv run eval/eval.py
+
+echo "Results:"
+cat eval/eval_data_mo-lunar-lander-v3.json
